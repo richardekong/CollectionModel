0a91f5443021e56d8110ca6b1276bdf73553c1e4
Richard <ekong.richard@yahoo.com>
2025-06-04 13:37:53 +0100

    initial project set up

---
+++ src/main/java/com/richard/collectionModel/CollectionModelApplication.java
@@ -0,0 +1,13 @@
+package com.richard.collectionModel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionModelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/richard/collectionModel/CollectionModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richard.collectionModel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
